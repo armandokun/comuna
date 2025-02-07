12b1fb5e0ad06666b482569bd4a50847863743c6
armandokun <armandasvaicikauskas@protonmail.com>
2025-02-07 10:12:40 +0200

    Add mock sign in

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,9 @@
-import { TouchableOpacity, View } from 'react-native'
+import { Platform, TouchableOpacity, View } from 'react-native'
 import { router, SplashScreen } from 'expo-router'
 import React, { ComponentProps, useContext, useEffect } from 'react'
 import { Ionicons } from '@expo/vector-icons'
 
+import { mockSignIn } from '@/libs/auth'
 import { signInWithApple } from '@/libs/apple'
 import { HOME } from '@/constants/routes'
 import Text from '@/components/ui/Text'
@@ -14,6 +15,7 @@ type Provider = {
   iconColor: string
   buttonColor: string
   onPress: () => void
+  show: boolean
 }
 
 const LoginScreen = () => {
@@ -41,22 +43,32 @@ const LoginScreen = () => {
 
   const PROVIDERS: Array<Provider> = [
     {
+      show: Platform.OS === 'ios',
       name: 'Apple',
       iconName: 'logo-apple',
       iconColor: 'white',
       buttonColor: 'black',
       onPress: signInWithApple,
     },
+    {
+      show: process.env.NODE_ENV === 'development',
+      name: 'Dev',
+      iconName: 'cog',
+      iconColor: 'white',
+      buttonColor: 'blue',
+      onPress: mockSignIn,
+    },
   ]
 
   return (
     <View className="flex-1 justify-center items-center">
       <View className="gap-4">
-        {PROVIDERS.map((provider) => (
+        {PROVIDERS.filter((provider) => provider.show).map((provider) => (
           <TouchableOpacity
             key={provider.name}
-            className="bg-black rounded-full p-4 flex-row items-center gap-2"
-            onPress={provider.onPress}>
+            className="rounded-full p-4 flex-row items-center gap-2"
+            onPress={provider.onPress}
+            style={{ backgroundColor: provider.buttonColor }}>
             <Ionicons name={provider.iconName} size={24} color={provider.iconColor} />
             <Text type="button">Sign in with {provider.name}</Text>
           </TouchableOpacity>
--- libs/auth.ts
+++ libs/auth.ts
@@ -5,7 +5,26 @@ import { supabase } from './supabase'
 export const signOut = async (onSignOut: () => void) => {
   const { error } = await supabase.auth.signOut()
 
-  if (error) Alert.alert(error.message)
+  if (error) {
+    Alert.alert(error.message)
+
+    return
+  }
 
   onSignOut()
+}
+
+export const mockSignIn = async () => {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: 'test@example.com',
+    password: 'password123',
+  })
+
+  if (error) {
+    Alert.alert('Error signing in:', error.message)
+
+    return
+  }
+
+  return data
 }
