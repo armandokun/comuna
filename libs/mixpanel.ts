4f94e909639e5561d036be825ca97b9aa28d032f
armandokun <armandasvaicikauskas@protonmail.com>
2025-02-27 14:02:46 +0200

    Opt out dev by default

--- libs/mixpanel.ts
+++ libs/mixpanel.ts
@@ -3,7 +3,9 @@ import { Mixpanel } from 'mixpanel-react-native'
 const trackAutomaticEvents = false
 const mixpanel = new Mixpanel(process.env.EXPO_PUBLIC_MIXPANEL_TOKEN!, trackAutomaticEvents)
 
-mixpanel.init(false, {
+const optOutTrackingByDefault = process.env.NODE_ENV === 'development'
+
+mixpanel.init(optOutTrackingByDefault, {
   autocapture: true,
 })
 
